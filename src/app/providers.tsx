69b191791e7e15acc0d3fbdcdebe5c02189e2915
Amal Mohammed <amal.cs71215@gmail.com>
2024-07-25 20:44:37 +0530

    Initialized the application

---
+++ src/app/providers.tsx
@@ -0,0 +1,12 @@
+// /src/app/providers.tsx
+'use client'
+
+import { ChakraProvider } from '@chakra-ui/react'
+import { theme } from '../chakra/theme'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+  <ChakraProvider theme={theme}>
+    {children}
+  </ChakraProvider>)
+}
