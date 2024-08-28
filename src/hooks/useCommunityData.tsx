b5e056ae42300112c3f60c76e909e449b1a11e24
Amal Mohammed <amal.cs71215@gmail.com>
2024-08-28 17:46:50 +0530

    Added functionality to join and leave communities and also only allow joining once authenticated

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,18 +1,78 @@
 // src/app/r/[communityId]/page.tsx
+import { authModalState } from "@/atoms/AuthModalAtom";
 import { Community, CommunitySnippet, communityState } from "@/atoms/communitiesAtom";
 import { auth, firestore } from "@/firebase/clientApp";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, doc, getDocs, increment, writeBatch } from "firebase/firestore";
 import { useCallback, useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const setAuthModalState = useSetRecoilState(authModalState);
   const [communityStateValue, setCommunityStateValue] = useRecoilState(communityState);
-  const joinCommunity = (communityData: Community) => {};
-  const leaveCommunity = (communityId: string) => {};
+  const joinCommunity = async (communityData: Community) => {
+    setLoading(true)
+    try {
+      // creating and adding a new communitySnippet to user
+      const batch = writeBatch(firestore);
+      const newSnippet: CommunitySnippet = {
+        communityId: communityData.id,
+        imageUrl: communityData.imageURL || "",
+      };
+      batch.set(doc(firestore, `users/${user?.uid}/communitySnippets`, communityData.id), newSnippet);
+
+      // updating the number of members on this community
+      batch.update(doc(firestore, 'communities', communityData.id), {
+        numberOfMembers: increment(1),
+      });
+      await batch.commit();
+
+      // update recoil state - communityState.mySnippets
+      setCommunityStateValue(prev => ({
+        ...prev,
+        mySnippets: [...prev.mySnippets, newSnippet],
+      }));
+
+    } catch (error: any) {
+      console.log('Community Join Error', error);
+      setError(error.message);
+    }
+    setLoading(false)
+  };
+  const leaveCommunity = async (communityId: string) => {
+    setLoading(true)
+    try {
+      // delelting communitySnippet from user
+      const batch = writeBatch(firestore);
+      batch.delete(doc(firestore, `users/${user?.uid}/communitySnippets`, communityId));
+
+      // updating the number of members on this community
+      batch.update(doc(firestore, 'communities', communityId), {
+        numberOfMembers: increment(-1),
+      });
+      await batch.commit();
+
+      // update recoil state - communityState.mySnippets
+      setCommunityStateValue(prev => ({
+        ...prev,
+        mySnippets: prev.mySnippets.filter(item => item.communityId !== communityId),
+      }));
+
+    } catch (error: any) {
+      console.log('Leave Community Error', error);
+      setError(error.message);
+    }
+    setLoading(false)
+  };
   
   const onJoinOrLeaveCommunity = (communityData: Community, isJoined: boolean) => {
-    // is the user signed in?
-      // if not open the auth modal to prompt them to sign in
+    // if user is not signed in, open the auth modal to prompt them to sign in
+    if (!user) {
+      // open modal
+      return setAuthModalState({ open: true, view: 'login' });
+    }
     
     // are they already part of this community
     if (isJoined) {
@@ -20,13 +80,9 @@ const useCommunityData = () => {
     }
     joinCommunity(communityData);
   };
-
-  const [loading, setloading] = useState(false);
-  const [error, setError] = useState('');
-  const [user] = useAuthState(auth);
-
+  
   const getMySnippets = useCallback(async () => {
-    setloading(true);
+    setLoading(true);
     try {
       const snippetDocs = await getDocs(collection(firestore, `users/${user?.uid}/communitySnippets`)); 
       const snippets = snippetDocs.docs.map(doc => ({ ...doc.data() }));
@@ -37,7 +93,7 @@ const useCommunityData = () => {
     } catch (error) {
       console.log('getMySnippet Error', error)
     }
-    setloading(false)
+    setLoading(false)
   }, [user, setCommunityStateValue]);
 
   useEffect(() => {
